5414193673bca5b9aacbd6c07041a109cbd2622d
mohamed <mohamayman770@gmail,com>
2023-10-27 01:00:39 +0200

    add: deploy workflow

---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,11 @@
+
+import {Typography} from '@mui/material'
+
+const NotFound = () => {
+  return <>
+  <Typography variant="h6" color="error">Not Fount This Page </Typography>
+  asdf
+  </>
+}
+
+export default NotFound
--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,25 @@
 
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, createHashRouter } from 'react-router-dom'
 import Home from './components/Home/Home.jsx'
 import Layout from './components/Layout/Layout.jsx'
 import Create from './components/Create/Create.jsx'
 import Profile from './components/Profile/Profile'
 import Settings from './components/Settings/Settings';
 import Logout from './components/Logout/Logout';
+import NotFound from './components/NotFound/NotFound';
 
 
 
-const router= createBrowserRouter([
-    {path:'',element:<Layout/>, children:[
+const router= createHashRouter ([
+    {path:'/',element:<Layout/>, children:[
         {index:true,element:<Home/>},
         {path:'/create',element:<Create/>},
         {path:'/profile',element:<Profile/>},
         {path:'/setting',element:<Settings/>},
         {path:'/logout',element:<Logout/>},
+        {path:'*',element:<NotFound/>},
 
     ]}
 ])
